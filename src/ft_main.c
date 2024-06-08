708eb20d299b47686dc08676801f7b57fe2a3cf4
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-06-08 21:38:57 +0900

    cmdが1つの場合

--- src/ft_main.c
+++ src/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:33:52 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2024/06/08 20:58:55 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/06/08 21:37:26 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,20 @@ int	main(int argc, char **argv, char **envp)
 		argc = 0;
 		ft_printf("%s\n", argv[0]);
 	}
-	//----------------------------------------------------------------
 	// エラー処理
-	if (argc < 4)
-	{
-		ft_printf_fd(STDERR_FILENO, "Usage: %s file1 cmd1 ... cmdn file2\n",
-			argv[0]);
-		return (1);
-	}
+	// if (argc < 4)
+	// {
+	// 	ft_printf_fd(STDERR_FILENO, "Usage: %s file1 cmd1 ... cmdn file2\n",
+	// 		argv[0]);
+	// 	return (1);
+	// }
+	//----------------------------------------------------------------
 	path_array = ft_gen_path_array(envp);
 
 	// ft_pipe(argv, path_array);
 	// argcいらなそう
 	// cmd_info.argc = argc;
+	// 6/8 argc==1でinfileが存在しない場合はここでreturnする
 	cmd_info.first_cmd = 2;
 	cmd_info.argv = argv;
 	cmd_info.path_array = path_array;
@@ -45,12 +46,12 @@ int	main(int argc, char **argv, char **envp)
 		ft_printf_fd(STDERR_FILENO, "%s: %s\n", argv[1], strerror(errno));
 	}
 	// ft_printf_fd(STDERR_FILENO, "argv[argc-1]: %s\n", argv[argc-1]);
+	// 6/8 argcが少なく、outfileが存在しない場合はここでreturnする
 	outfile_fd = open(argv[argc-1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	//outfileがひらけなかった時のエラーメッセージ
 	if (outfile_fd == -1)
 		ft_printf_fd(STDERR_FILENO, "%s: %s\n", argv[argc - 1], strerror(errno));
-	// ft_printf_fd(STDERR_FILENO, "out_fd: %d\n", cmd_info.outfile_fd);
-	// ft_recursive(cmd_info, argc - 2, outfile_fd); //cmd_count:最後に実行するコマンドの場所 argc-2、out_fd:outfileのfd
+	// 6/8 argc==3で実行するコマンドが一つもない場合はここでreturnする
 	ft_pipe(cmd_info, argc - 2, outfile_fd); //cmd_count:最後に実行するコマンドの場所 argc-2、out_fd:outfileのfd
 	ft_printf_fd(STDERR_FILENO, "come back to main\n");
 
--- src/ft_pipe.c
+++ src/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 11:53:17 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2024/06/08 20:58:51 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/06/08 21:35:34 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,6 @@ void	ft_pipe(t_cmd_info cmd_info, int cmd_count, int out_fd)
 	int pipe_fd[2];
 	pid_t pid;
 
-	//最初以外のコマンド
 	pipe(pipe_fd);
 	pid = fork();
 	if (pid == -1)
@@ -84,12 +83,21 @@ void	ft_pipe(t_cmd_info cmd_info, int cmd_count, int out_fd)
 	}
 	if (pid == 0)//子プロセス
 	{
-		ft_recursive(cmd_info, cmd_count - 1, pipe_fd[1]);
+		//最後のコマンドの入力を決める
+		//最後のコマンドが最初のコマンドでもある場合（コマンドが1個）
+		if (cmd_count == cmd_info.first_cmd)
+		{
+			dup2(cmd_info.infile_fd, STDIN_FILENO);
+			close(cmd_info.infile_fd);
+		}
+		else
+		{
+			ft_recursive(cmd_info, cmd_count - 1, pipe_fd[1]);
+			close(pipe_fd[1]);
+			dup2(pipe_fd[0], STDIN_FILENO);
+			close(pipe_fd[0]);
+		}
 		//最後のコマンドを実行する、exitはしない
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], STDIN_FILENO);
-		close(pipe_fd[0]);
-		// ft_printf_fd(STDERR_FILENO, " out_fd: %d\n", out_fd);
 		if (out_fd > 0)
 		{
 			dup2(out_fd, STDOUT_FILENO);
